ab3d1d2c429d54b4b2653da5bd1250883dc7de0f
Hiroyuki MITSUBORI <hmitubor@gmail.com>
2014-11-06 01:17:35 +0900

    Change secret parameter to hidden file.

--- BDScheduler/src/BDScheduler.hx
+++ BDScheduler/src/BDScheduler.hx
@@ -1,21 +1,19 @@
 package;
 
-//import oauth.OAuth2;
 import oauth.Client;
 import oauth.Tokens;
+import Secret;
 
 class BDScheduler {
 	static var auth_url = "https://accounts.google.com/o/oauth2/auth";
 	static var token_url = "https://accounts.google.com/o/oauth2/token";
-	static var client_key = "864839047331-9cmf8rebl3mckrd3gm4sjr1cm5qmqefv.apps.googleusercontent.com";
-	static var client_secret = "pZz5u_vP4YJY_Orkhn1MmDlY";
 	static var redirect_uris = "urn:ietf:wg:oauth:2.0:oob";
 	static var scope_uri = "https://www.googleapis.com/auth/calendar";
 //	static var auth_code = "4/7JAdiE4iq8XcPklnz6B8zs3V3RxzttsR66pBKQI7jvY.YiOKtADOBPobBrG_bnfDxpKqcx_YkgI";
 
 	static function getAuthCode():String {
 	//CLIENT Open window to show user the authentication screen
-		var auth_url_ret = oauth.OAuth2.buildAuthUrl(auth_url, client_key, 
+		var auth_url_ret = oauth.OAuth2.buildAuthUrl(auth_url, Secret.client_key, 
 				{ redirectUri:redirect_uris,
 					scope:scope_uri,
 					state:oauth.OAuth2.nonce() });
@@ -27,13 +25,13 @@ class BDScheduler {
 
 	static function makeClientViaAccessToken(accessToken:String, refreshToken:String):Client2 {
 		var expires = 3600;
-		return MyOAuth2.connect(new oauth.Consumer(client_key, client_secret),
+		return MyOAuth2.connect(new oauth.Consumer(Secret.client_key, Secret.client_secret),
 					new OAuth2AccessToken(accessToken, expires),
 					new RefreshToken(refreshToken));
 	}
 
 	static function makeClientViaAuthCode(authCode:String):Client2 {
-		var consumer = MyOAuth2.connect(new oauth.Consumer(client_key, client_secret));
+		var consumer = MyOAuth2.connect(new oauth.Consumer(Secret.client_key, Secret.client_secret));
 		return consumer.getAccessToken2(token_url, authCode, redirect_uris);
 	}
 
@@ -53,9 +51,7 @@ class BDScheduler {
 
 //		var client = makeClientViaAuthCode( getAuthCode() );
 
-		var acc_token = "ya29.tAAhYIN8MASkE-UCSdchMdFVt5NVzFNvQqLWNn5nMoF9GqGkiyT3s8vBYGXxaBBmJae0-uejAqeKBw";
-		var ref_token = "1/T2Eu63fQMULbaVwJiip8YqjGB4Me0FW_I25tNbEYFww";
-		var client = makeClientViaAccessToken(acc_token, ref_token);
+		var client = makeClientViaAccessToken(Secret.acc_token, Secret.ref_token);
 	
 		//SERVER Get a new access token if the old one has expired
 		client.refreshAccessToken(token_url);
@@ -70,7 +66,7 @@ class BDScheduler {
 		var   end_time = DateTools.format(new Date(2014, 10, 3, 18, 30, 00), "%Y-%m-%dT%T+09:00");
 		
 		trace(client.requestJSONforGoogle("https://www.googleapis.com/calendar/v3/calendars/hmitubor@gmail.com/events"
-					+ "?key=" + client_key,
+					+ "?key=" + Secret.client_key,
 					true,
 					'{
  "start": {
